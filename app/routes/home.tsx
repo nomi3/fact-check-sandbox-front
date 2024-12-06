93ec21825a9e0e8aedee7966c5ec8c688801ce4c
nomi3 <thisisnomi3@gmail.com>
2024-12-06 23:35:11 +0900

    Add cache

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Fact Check Sandbox" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
